4aae0dd11269baa09c28a117f840ab139912bfb5
randommarcus <sudarshan.nslr@gmail.com>
2025-07-24 18:07:01 +0530

    Create recommendation function

--- app.py
+++ app.py
@@ -23,3 +23,26 @@ tfidf_matrix = tfidf.fit_transform(df['combined_features'])
 
 # Compute the cosine similarity matrix
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+# Create a Series with movie titles as index and DataFrame indices as values
+indices = pd.Series(df.index, index=df['name']).drop_duplicates()
+
+def get_recommendations(name, cosine_sim=cosine_sim):
+    # Get the index of the movie that matches the name
+    idx = indices[name]
+
+    # Get the pairwise similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return df['name'].iloc[movie_indices]
+
